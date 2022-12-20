d07962ef305e9e72482e45dedf7d43b7c3eaab5b
michiel1106 <107875826+michiel1106@users.noreply.github.com>
2022-12-20 17:57:07 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,78 +1,60 @@
 import time
 
+variable1 = float(46)
+variable2 = float(2.89)
+
+print(variable1 / variable2)
+
+
+
 print("hello! this is a calculator written in python")
 time.sleep(1)
 
 print(
-"""
-What do you want to calculate?
-1. +
-2. -
-3. *
-4. /
-"""
+    """
+    What do you want to calculate?
+    1. +
+    2. -
+    3. *
+    4. /
+    5. BMI
+    """
 )
 
 calculateanswer = input(">")
 
 if calculateanswer == "1":
-    print(
-"""alright!
-
-please input your first number(s)"""
-    )
-    firsinputadding = int(input())
-
-    print("please add you second input")
-    time.sleep(1)
-    secondinputadding = int(input())
-
-    answeradding = int(firsinputadding) + int(secondinputadding)
-    print(answeradding)
-
-if calculateanswer == "3":
-    print(
-"""multiplication! alright!
-
-please input your first number(s)"""
-    )
-    firsinputmultiplication = int(input())
-
-    print("please add you second input")
-    time.sleep(1)
-    secondinputmultiplication = int(input())
-
-    answermultiplication = int(firsinputmultiplication) * int(secondinputmultiplication)
-    print(answermultiplication)
-
-
-if calculateanswer == "2":
-    print(
-"""minus! okay
-
-please input your first number(s)"""
-    )
-    firsinputminus = int(input())
-
-    print("please add you second input")
-    time.sleep(1)
-    secondinputminus = int(input())
-
-    answerminus = int(firsinputminus) - int(secondinputminus)
-    print(answerminus)
+    print("please input your first number(s)")
+    first_input_adding = input(">")
+    print("please input your second number(s)")
+    second_input_adding = input(">")
+    print(int(first_input_adding) + int(second_input_adding))
+elif calculateanswer == "2":
+    print("please input your first number(s)")
+    first_input_minus = input(">")
+    print("please input your second number(s)")
+    second_input_minus = input(">")
+    print(int(first_input_minus) - int(second_input_minus))
+elif calculateanswer == "3":
+    print("please input your first number(s)")
+    first_input_multiply = input(">")
+    print("please input your second number(s)")
+    second_input_multiply = input(">")
+    print(int(first_input_multiply) * int(second_input_multiply))
+elif calculateanswer == "4":
+    print("please input your first number(s)")
+    first_input_divide = input(">")
+    print("please input your second number(s)")
+    second_input_divide = input(">")
+    print(int(first_input_divide) / int(second_input_divide))
+elif calculateanswer == "5":
+    print("please input your weight")
+    weight = float(input(">"))
+    print("please input your length. Like 1.70 meters, not 170 meters, not 1,70, but 1.70")
+    length = float(input(">"))
+    lenth_x_length = length * length
+    output_bmi = weight / lenth_x_length
+    print(output_bmi)
 
 
-if calculateanswer == "4":
-    print(
-"""uuuh idk.. doing that alright!
 
-please input your first number(s)"""
-    )
-    firsinputidk = int(input())
-
-    print("please add you second input")
-    time.sleep(1)
-    secondinputidk = int(input())
-
-    answeridk = int(firsinputidk) / int(secondinputidk)
-    print(answeridk)
